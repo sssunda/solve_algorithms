cae0d520a55eebef14872a36a566ffb4f24e1711
sssunda <sundaeun93@gmail.com>
2019-09-04 23:31:43 +0900

    try to solve [무지의 먹방라이브]. I have to solve Runtime Error

--- kakao_blind_recruitment_2018/live.py
+++ kakao_blind_recruitment_2018/live.py
@@ -1,34 +1,27 @@
 def solution(food_times, k):
-    return eat(food_times, 0, 0, k, 1)
+    return eat(food_times, 0, 0, k)
 
 
-def eat(food_time, idx, cnt, k, direction):
+def eat(food_time, idx, cnt, k):
     if idx > len(food_time) -1:
         idx = 0
-    elif idx < 0:
-        idx = len(food_time) + idx
-
+        
     if cnt == k:
+        if food_time[idx] == 0 :
+            return eat(food_time, idx+1, cnt, k)
         return idx+1
     elif cnt != k and sum(food_time) == 0 :
         return -1
-
-    food_time[idx] = food_time[idx]-1
-    temp = idx + direction
-    if temp > len(food_time) -1:
-        temp = 0
-    elif temp < 0:
-        temp = len(food_time) - temp
-
-    if food_time[temp] == 0:
-        direction *= (-1)
+    
+    if food_time[idx] == 0:
+        return eat(food_time, idx+1, cnt, k)
                 
-    idx += direction
-    return eat(food_time, idx, cnt+1, k, direction)
+    food_time[idx] = food_time[idx]-1
+    return eat(food_time, idx+1, cnt+1, k)
 
 
 if __name__ == '__main__':
     food_times = [3,1,1,1,2,4,3]
     # food_times = [3,1,2]
-    k =12
+    k = 12
     print(solution(food_times, k))
