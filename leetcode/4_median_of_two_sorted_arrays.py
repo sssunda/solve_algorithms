1e9ad6071a4d3fca169e7023b229b566bbb68d39
sssunda <sundaeun93@gmail.com>
2020-03-07 22:24:01 +0900

    solve algorithm [median_of_two_sorted_arrays] in leetcode

---
+++ leetcode/4_median_of_two_sorted_arrays.py
@@ -0,0 +1,33 @@
+"""
+
+4. Median of Two Sorted Arrays
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+
+"""
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums = nums1 + nums2
+        nums.sort()
+        nums_len = len(nums)
+        n = nums_len // 2
+        if nums_len % 2 == 0:
+            return sum(nums[n-1:n+1]) / 2
+        else:
+            return nums[n]
